ffb7588ca076c9a3dadf4cb72a0655979ceeecdb
Célian Garcia <celian.garcia1@gmail.com>
2019-10-13 09:25:10 +0000

    Deploy model on push on master

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+MODEL_NAME="default"
+MODEL_FILE="main.json"
+CEREBRO_URL="https://milobella.com/cerebro"
+
+echo "Checking cerebro status..."
+echo " ==> curl --write-out %{http_code} --silent --output /dev/null ${CEREBRO_URL}/models/${MODEL_NAME}/train"
+response=$(curl --write-out %{http_code} --silent --output /dev/null ${CEREBRO_URL}/models/${MODEL_NAME}/train)
+
+if [ $response = "200" ]
+then
+    echo "Cerebro is available !"
+else
+    echo "Cerebro is unavailable (${response}), please try again in a few minutes."
+    exit 1
+fi
+
+echo "Uploading the model ${MODEL_NAME} from the file ${MODEL_FILE} ..."
+echo " ==> curl -X PUT \"${CEREBRO_URL}/models/${MODEL_NAME}/samples\" -d @${MODEL_FILE}"
+curl -X PUT "${CEREBRO_URL}/models/${MODEL_NAME}/samples" -d @${MODEL_FILE} || exit 1
+echo "Model uploaded !"
+
+echo "Training the model ${MODEL_NAME} ..."
+echo " ==> curl -X POST \"${CEREBRO_URL}/models/${MODEL_NAME}/train\""
+curl -X POST "${CEREBRO_URL}/models/${MODEL_NAME}/train" || exit 1
+echo "Model train triggered !"
+
+
+attempt_counter=0
+max_attempts=18 # Wait for 5 min max
+
+until [ $(curl --write-out %{http_code} --silent --fail --output /dev/null ${CEREBRO_URL}/models/${MODEL_NAME}/train) = "200" ]; do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached, check the cerebro instance."
+      exit 1
+    fi
+
+    printf '.'
+    attempt_counter=$(($attempt_counter+1))
+    sleep 10
+done
+
+echo "Model has been successfully deployed !"

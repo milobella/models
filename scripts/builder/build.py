7273af8dea23240733a47bffc215082f318e7f38
Celian Garcia <celian.garcia1@gmail.com>
2021-03-23 08:12:27 +0100

    clean(#5): Refactored the code before handling entities

--- scripts/builder/build.py
+++ scripts/builder/build.py
@@ -31,26 +31,48 @@ class HumanIntent:
         return "{name}{sentences}".format(name=self.name, sentences=self.sentences)
 
 
-class HumanIntentReader:
+class FilesReader:
     """
     Read the human readable intents from the input folder. Each intent file should have this format :
     <INTENT_NAME>.yaml and contains the list of sentences in the yaml format.
     """
     _data_folder: str
+    _manifest: Manifest
 
-    def __init__(self, data_folder: str):
+    def __init__(self, data_folder: str, manifest_file: str):
         self._data_folder = data_folder
+        self._manifest = FilesReader.read_manifest(manifest_file)
+
+    @staticmethod
+    def read_manifest(file) -> Manifest:
+        """
+        Load manifest file
+        :param file:
+        :return:
+        """
+        document = yaml.load(file, Loader=yaml.FullLoader)
+        return Manifest(document["intents"])
 
     def _build_path(self, file) -> str:
         return Path(self._data_folder) / file.name
 
-    def build(self, file) -> HumanIntent:
+    def read_intent(self, file) -> HumanIntent:
         with open(self._build_path(file)) as f:
             sentences = yaml.load(f, Loader=yaml.FullLoader)
             return HumanIntent(
                 name=os.path.splitext(file.name)[0],
                 sentences=sentences
             )
+
+    def read_intents(self):
+        intents = []
+        for f in os.scandir(self._data_folder):
+            intent = self.read_intent(f)
+            if f.is_file() and intent.name in self._manifest.intents:
+                intents.append(intent)
+            else:
+                del intent
+        return intents
 
 
 class CerebroEntity:
@@ -88,54 +110,44 @@ class CerebroDataModel(List[CerebroDataModelEntry]):
     pass
 
 
-def read_manifest(file) -> Manifest:
-    """
-    Load manifest file
-    :param file:
-    :return:
-    """
-    document = yaml.load(file, Loader=yaml.FullLoader)
-    return Manifest(document["intents"])
+class CerebroFactory:
+    def __init__(self):
+        pass
 
+    @staticmethod
+    def build_entry(name: str, sentence: str) -> CerebroDataModelEntry:
+        return CerebroDataModelEntry(
+            text=sentence,
+            categories=[name],
+            entities=[]  # TODO: Read entities from xml
+        )
 
-def generate_data_model(intents: List[HumanIntent]) -> CerebroDataModel:
-    """
-    Generate the Cerebro backend's model, from the intents parsed.
-    :return:
-    """
-    model = CerebroDataModel()
+    @staticmethod
+    def build_data_model(intents: List[HumanIntent]) -> CerebroDataModel:
+        """
+        Generate the Cerebro backend's model, from the intents parsed.
+        :return:
+        """
+        model = CerebroDataModel()
 
-    for intent in intents:
-        for sentence in intent.sentences:
-            model.append(CerebroDataModelEntry(
-                text=sentence,
-                categories=[intent.name],
-                entities=[]  # TODO: Read entities from xml
-            ))
+        for intent in intents:
+            for sentence in intent.sentences:
+                model.append(CerebroFactory.build_entry(intent.name, sentence))
 
-    return model
+        return model
 
 
 def main(data_folder, manifest_file, output_file):
-    manifest = read_manifest(manifest_file)
-
     # Read human readable intent's files which are in the manifest.
-    intents = []
-    intent_builder = HumanIntentReader(data_folder)
-    for f in os.scandir(data_folder):
-        intent = intent_builder.build(f)
-        if f.is_file() and intent.name in manifest.intents:
-            intents.append(intent)
-        else:
-            del intent
+    intent_builder = FilesReader(data_folder, manifest_file)
+    intents = intent_builder.read_intents()
 
     # Generate the data model for the Cerebro's backend from these intents, and print it into the output file.
-    data_model = generate_data_model(intents)
+    data_model = CerebroFactory.build_data_model(intents)
     json.dump(data_model, output_file, default=lambda o: o.__dict__)
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='Build Cerebro model.')
     parser.add_argument('--data', metavar='DATA_FOLDER', type=str,
                         help='The data folder containing intent files in the format <INTENT_NAME>.yaml.')

27bfcfc054a6654723987b869dd5b51f6c9b2a2e
Celian Garcia <celian.garcia1@gmail.com>
2022-10-09 15:35:09 +0200

    fix(#10): Can generate model sentence with several entities

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3 scripts/builder/build.py --data ./data --manifest ./manifest_default.yaml --output ./output/default.json
+python scripts/builder/build.py --data ./data --manifest ./manifest_default.yaml --output ./output/default.json
